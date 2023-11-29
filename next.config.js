573144d49596e34fb12cd181538064f5dd12fb69
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2023-11-29 12:59:45 +0800

    change the app file to pages file name and page,js change to index.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Define publicRuntimeConfig for runtime configuration
+    publicRuntimeConfig: {
+      // Add your path or any other configuration
+      basePath: 'pages/index',
+    },
+  
+    // You can add more configurations here based on your requirements
+  }
+  
+  module.exports = nextConfig;  
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to our website!</h1>
+      <a href="/layout">click me</a>
+    </div>
+  )
+}
