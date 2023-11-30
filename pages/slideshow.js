be827a78caff9273aa334559d2a720d1f8e9b267
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2023-11-30 22:41:28 +0800

    slideshow done

--- pages/slideshow.js
+++ pages/slideshow.js
@@ -1,42 +1,39 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
-const styles = `
-/* Slideshow */
-.slideshow {
+const Slideshow = styled.div`
   margin-top: 15px;
   margin-bottom: 15px;
-  margin-left: 80px;
-//   margin-right: auto;
+  margin-left: 30px;
   overflow: hidden;
   width: 712px;
   height: auto;
   z-index: 1;
-}
+`;
 
-.slideshowSlider {
+const SlideshowSlider = styled.div`
   white-space: nowrap;
   transition: ease 1000ms;
-  margin-top: 80px;
-}
+  margin-top: 30px;
+  transform: translate3d(${props => -props.index * 100}%, 0, 0);
+`;
 
-.slide {
+const Slide = styled.img`
   display: inline-block;
   height: 356px;
   width: 100%;
   border-radius: 40px;
-}
+`;
 
-/* Buttons */
-.slideshowDots {
-  margin-top: -45px;
+const SlideshowDots = styled.div`
+  margin-top: -48px;
   position: absolute;
   z-index: 3;
   width: 712px;
   text-align: center;
-}
+`;
 
-.slideshowDot {
+const SlideshowDot = styled.div`
   display: inline-block;
   height: 20px;
   width: 20px;
@@ -46,12 +43,7 @@ const styles = `
   margin-right: 7px;
   margin-bottom: 0px;
   margin-left: 7px;
-  background-color: #c4c4c4;
-}
-
-.slideshowDot.active {
-  background-color: #6a0dad;
-}
+  background-color: ${props => (props.index === props.i ? '#6a0dad' : '#c4c4c4')};
 `;
 
 // Image Array URLs
@@ -60,7 +52,7 @@ const imageUrls = ["SlideShow/image1.png", "SlideShow/image2.png", "SlideShow/im
 // Delay between slides
 const delay = 2500;
 
-export default function Slideshow() {
+export default function SlideShow() {
   const [index, setIndex] = React.useState(0);
   const isReversed = React.useRef(false);
 
@@ -91,28 +83,16 @@ export default function Slideshow() {
 
   return (
     <div>
-      <style>{styles}</style>
-      <div className="slideshow">
-        <div
-          className="slideshowSlider"
-          style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-        >
-          {imageUrls.map((imageUrl, i) => (
-            <img className="slide" key={i} src={imageUrl} alt={`Slide ${i + 1}`} />
-          ))}
-        </div>
-        <div className="slideshowDots">
-          {imageUrls.map((_, i) => (
-            <div
-              key={i}
-              className={`slideshowDot${index === i ? " active" : ""}`}
-              onClick={() => {
-                setIndex(i);
-              }}
-            ></div>
-          ))}
-        </div>
-      </div>
+        <Slideshow>
+            <SlideshowSlider index={index}>
+                {imageUrls.map((imageUrl, i) => (<Slide key={i} src={imageUrl} alt={`Slide ${i + 1}`} />))}
+            </SlideshowSlider>
+            <SlideshowDots>
+                {imageUrls.map((_, i) => (
+                    <SlideshowDot key={i} index={index} i={i} onClick={() => {setIndex(i);}}></SlideshowDot>
+                ))}
+            </SlideshowDots>
+        </Slideshow>
     </div>
   );
 }
