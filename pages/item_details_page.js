932bc681c657dae2fca807145c9f2f9341395438
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2023-12-06 21:18:30 +0800

    details page done

--- pages/item_details_page.js
+++ pages/item_details_page.js
@@ -11,7 +11,7 @@ const Container = styled.div`
     margin-right: auto;
     margin-top: 90px;
     width: 95%;
-    height: 1965px;
+    height: fit-content;
     background: #F8F9FA;
     padding: 20px 10px;
 `;
@@ -313,7 +313,7 @@ const BtnCon = styled(BtnRow)`
     align-items: center;
     text-align: center;
 
-    margin: 35px 0 0 0;
+    margin: 20px 0 0 0;
     
     /* background-color: red; */
 `;
@@ -323,14 +323,125 @@ const SharedBtn = styled(Button)`
 `;
 
 const AddBtn = styled(SharedBtn)`
-    margin-right: 10px;
+    margin-right: 15px;
+
+    &:hover{
+        border: 1px solid #F73636;
+        background: #FFBCBC;
+        color: #000;
+    }
 `;
 
 const BuyBtn = styled(SharedBtn)`
-    margin-left: 10px;
+    margin-left: 15px;
+
+    &:hover{
+        background: #F73636;
+        border: none;
+    }
+`;
+
+const IDBtn = styled(Button)`
+    font-weight: 600;
+`;
+
+const Input = styled.input`
+    text-align: center;
+    font-size: 16px;
+    font-weight: 500;
+    font-family: Inter;
+
+    min-width: 10%;
+    height: 35px;
+    padding: 0;
+    margin-right: 10px;
+    border: none;
+    background-color: transparent;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+    }
 `;
 
 // Right Ship End
+
+// Bottom Start
+const BtmContainer = styled(Container)`
+    margin-top: 0;
+    background: #F8F9FA;
+    width: 95%;
+    justify-content: center;
+`;
+
+const DecpCon = styled.div`
+    width: 90%;
+    height: fit-content;
+    border-radius: 8px;
+    border: 1px solid #6C757D;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.h2`
+    color: #000;
+    font-family: Inter;
+    font-size: 28px;
+    font-weight: 400;
+
+    border-radius: 8px 8px 0px 0px;
+    background: #E9ECEF;
+    height: 80px;
+    width: 98%;
+    display: flex;
+    align-items: center;
+    margin: 0;
+    margin-bottom: 20px;
+    padding-left: 2%;
+`;
+
+const TextCon = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin: 0 0 20px 0;
+    padding: 0 0 0 20px;
+`;
+
+const IconCon = styled.div`
+    width: 5%;
+    padding-top: 5px;
+    text-align: center;
+`;
+
+const IconImg = styled.img`
+`;
+
+const Spec = styled.h2`
+    width: 15%;
+    color: #6C757D;
+
+    font-family: Inter;
+    font-size: 24px;
+    font-weight: 400;
+    margin: 0;
+    padding: 0;
+
+`;
+
+const Text = styled.p`
+    width: 50%;
+
+    font-family: Inter;
+    font-size: 24px;
+    font-weight: 400;
+    margin: 0;
+    padding: 0;
+    flex-wrap: wrap;
+`;
+
+// Bottom End
+
 export default function DetailPage(){
     const [slideIndex, setSlideIndex] = useState(0);
 
@@ -342,7 +453,7 @@ export default function DetailPage(){
         setSlideIndex((prevIndex) => prevIndex + n);
       };
     
-      const showSlides = () => {
+    const showSlides = () => {
         const slides = document.querySelector('.slides');
         const totalSlides = slides.children.length;
         const maxIndex = totalSlides - 4; // Displaying 4 slides at a time
@@ -356,7 +467,31 @@ export default function DetailPage(){
     
         const translateValue = -slideIndex * 25; // 25% width for each slide
         slides.style.transform = `translateX(${translateValue}%)`;
-      };
+    };
+        
+    const [value, setValue] = useState(0);
+    const min = 0;
+    const max = 999;
+
+    const increment = () => {
+        if (value < max) {
+            setValue(value + 1);
+        }
+    };
+
+    const decrement = () => {
+        if (value > min) {
+            setValue(value - 1);
+        }
+    };
+
+    const handleChange = (e) => {
+        let inputValue = e.target.value;
+
+        if (inputValue >= min && inputValue <= max) {
+            setValue(inputValue);
+        }
+    };
 
     return (
         <div>
@@ -453,19 +588,6 @@ export default function DetailPage(){
 
                         <SubCon>
                             <BtnRow>
-
-                                <Button>Pink</Button>
-                                <Button>Blue</Button>
-                                <Button>Purple</Button>
-                                <Button>Violet</Button>
-                                <Button>Black Titanium</Button>
-
-                                <Button>Pink</Button>
-                                <Button>Blue</Button>
-                                <Button>Purple</Button>
-                                <Button>Violet</Button>
-                                <Button>Black Titanium</Button>
-
                                 <Button>Pink</Button>
                                 <Button>Blue</Button>
                                 <Button>Purple</Button>
@@ -481,12 +603,24 @@ export default function DetailPage(){
 
                         <SubCon>
                             <BtnRow>
-
                                 <Button>64 GB</Button>
                                 <Button>128 GB</Button>
                                 <Button>256 GB</Button>
                                 <Button>512 GB</Button>
                                 <Button>1 TB</Button>
+                            </BtnRow>
+                        </SubCon>
+                    </DetailCon>
+
+                    <DetailCon>
+                        <ImgCon><SelectionIcon src="Item/Qty_Icon.png" alt="Ship" /></ImgCon>
+                        <SelectTxt>Quantity</SelectTxt>
+
+                        <SubCon>
+                            <BtnRow>
+                                <IDBtn onClick={increment}>+</IDBtn>
+                                <Input type="number" min={0} max={999} value={value} onChange={handleChange} />
+                                <IDBtn onClick={decrement}>-</IDBtn>
                             </BtnRow>
                         </SubCon>
                     </DetailCon>
@@ -498,6 +632,82 @@ export default function DetailPage(){
 
                 </RightCon>
             </Container>
+
+            <BtmContainer>
+                <DecpCon>
+                    <Title>Product Specification</Title>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Processor</Spec>
+                        <Text>A15 Bionic</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>OS</Spec>
+                        <Text>IOS</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Weight</Spec>
+                        <Text>179g</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Dimension</Spec>
+                        <Text>Abcdefg</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Display</Spec>
+                        <Text>OLED</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Memory</Spec>
+                        <Text>64GB / 128GB / 256GB / 512GB</Text>
+                    </TextCon>
+                    
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>NFC</Spec>
+                        <Text>YES</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Camera</Spec>
+                        <Text>
+                            48 MP, f/1.6, 26mm (wide), 1/1.56", 1.0µm, dual pixel PDAF, sensor-shift OIS
+                            12 MP, f/2.4, 13mm, 120˚ (ultrawide)
+                        </Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Color</Spec>
+                        <Text>Pink / Blue / Violet</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>Loud Speaker</Spec>
+                        <Text>Yes, with stereo speakers</Text>
+                    </TextCon>
+
+                    <TextCon>
+                        <IconCon><IconImg src="Item/Moon.png" alt="Icon"/></IconCon>
+                        <Spec>3.5mm jack</Spec>
+                        <Text>No</Text>
+                    </TextCon>
+
+                </DecpCon>
+            </BtmContainer>
         </div>
     )
 }
